bbbd440d79dc9c369a4de913c27cbaa014ce5efd
Deadloct <rad@deadloct.com>
2024-06-20 15:59:22 +1000

    Move to bhguides.com

--- src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./index.module.css";
-
-export default function Footer() {
-    return (
-        <footer className={styles["site-footer"]}>
-            <p>This site is not affiliated in any way with Kongregate, Juppiomenz, or Bit Heroes.</p>
-        </footer>
-    );
-}
